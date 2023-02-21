561794524b9d8780fd2f10251c02e66601906a32
Julie Brunet <juliebrunet21@msn.com>
2023-02-21 18:09:48 +0100

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   TYPES = %w[chinese italian japanese french belgian].freeze
-  validates :category, inclusion: { in: TYPES }
+  validates :category, inclusion: { in: TYPES }, presence: true
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, presence: true
 end
